8ed77aaf87724b23e73bc5b9329fda4eb1b21d31
mrdakj <mrdakj@gmail.com>
2018-12-28 19:28:52 +0100

    name clusters replaced with centroids

--- src/kmeans.cpp
+++ src/kmeans.cpp
@@ -9,32 +9,32 @@ int main()
 	img::Image<img::Type::RGB> output(img.rows(), img.cols());
 
 	int k = 10;
-	std::vector<cv::Vec3i> clusters(k);
+	std::vector<cv::Vec3i> centroids(k);
 
-	clusters[0] = std::accumulate(img.begin(), img.end(), cv::Vec3i(0,0,0), [](cv::Vec3i s, cv::Vec3i i) { return s+i; }) / (img.rows()*img.cols());
+	centroids[0] = std::accumulate(img.begin(), img.end(), cv::Vec3i(0,0,0), [](cv::Vec3i s, cv::Vec3i i) { return s+i; }) / (img.rows()*img.cols());
 
 	for (int i = 1; i < k; i++) {
 		int maxd = 0;
 
 		for (cv::Vec3i p : img) {
-			auto it = std::min_element(clusters.begin(), clusters.begin()+i, [&p](auto&& a, auto&& b) { return (a-p).dot(a-p) < (b-p).dot(b-p); });
+			auto it = std::min_element(centroids.begin(), centroids.begin()+i, [&p](auto&& a, auto&& b) { return (a-p).dot(a-p) < (b-p).dot(b-p); });
 
 			int d = (p-*it).dot(p-*it);
 
 			if (d > maxd) {
-				clusters[i] = p;
+				centroids[i] = p;
 				maxd = d;
 			}
 		}
 	}
 
-	for (auto&& p : clusters) {
+	for (auto&& p : centroids) {
 		std::cout << p << std::endl;
 	}
 	std::cout << "----------------------" << std::endl;
 
 	for (int i = 0; i < MAX_ITER; i++) {
-		std::vector<cv::Vec3i> new_clusters(k, cv::Vec3i(0,0,0));
+		std::vector<cv::Vec3i> new_centroids(k, cv::Vec3i(0,0,0));
 		std::vector<int> counter(k, 0);
 
 		
@@ -42,27 +42,27 @@ int main()
 
 		// unsigned long long f = 0;
 		for (cv::Vec3i p : img) {
-			int m = std::distance(clusters.begin(), 
-								std::min_element(clusters.begin(), clusters.end(), [&p](auto&& a, auto&& b) { return (a-p).dot(a-p) < (b-p).dot(b-p); })
+			int m = std::distance(centroids.begin(), 
+								std::min_element(centroids.begin(), centroids.end(), [&p](auto&& a, auto&& b) { return (a-p).dot(a-p) < (b-p).dot(b-p); })
 					);
 
-			// f += (p-clusters[m]).dot(p-clusters[m]);
-			new_clusters[m] += p;
+			// f += (p-centroids[m]).dot(p-centroids[m]);
+			new_centroids[m] += p;
 			counter[m]++;
-			*it++ = clusters[m];
+			*it++ = centroids[m];
 		}
 
 		for (int j = 0; j < k; j++) {
 			if (counter[j] != 0)
-				new_clusters[j] /= counter[j];
+				new_centroids[j] /= counter[j];
 		}
 
-		if (std::inner_product(clusters.begin(), clusters.end(), new_clusters.begin(), 0ull, std::plus<>(), [](auto&& lhs, auto&& rhs) { return (lhs-rhs).dot(lhs-rhs); }) < 5)
+		if (std::inner_product(centroids.begin(), centroids.end(), new_centroids.begin(), 0ull, std::plus<>(), [](auto&& lhs, auto&& rhs) { return (lhs-rhs).dot(lhs-rhs); }) < 5)
 			break;
 
 		// std::cout << f << std::endl;
 
-		clusters = std::move(new_clusters);
+		centroids = std::move(new_centroids);
 	}
 
 	output.show();
