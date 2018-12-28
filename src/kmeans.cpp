71f3792f88f58debf8799d40d807c367f585e84b
mrdakj <mrdakj@gmail.com>
2018-12-28 19:27:02 +0100

    convergence is ok in kmeans (unsigned replaced with unsigned long long)

--- src/kmeans.cpp
+++ src/kmeans.cpp
@@ -5,10 +5,10 @@
 
 int main()
 {
-	img::Image<img::Type::RGB> img("images/mat.jpg");
+	img::Image<img::Type::RGB> img("images/cars.jpg");
 	img::Image<img::Type::RGB> output(img.rows(), img.cols());
 
-	int k = 7;
+	int k = 10;
 	std::vector<cv::Vec3i> clusters(k);
 
 	clusters[0] = std::accumulate(img.begin(), img.end(), cv::Vec3i(0,0,0), [](cv::Vec3i s, cv::Vec3i i) { return s+i; }) / (img.rows()*img.cols());
@@ -40,14 +40,16 @@ int main()
 		
 		auto it = output.begin();
 
+		// unsigned long long f = 0;
 		for (cv::Vec3i p : img) {
-				int m = std::distance(clusters.begin(), 
-									std::min_element(clusters.begin(), clusters.end(), [&p](auto&& a, auto&& b) { return (a-p).dot(a-p) < (b-p).dot(b-p); })
-						);
+			int m = std::distance(clusters.begin(), 
+								std::min_element(clusters.begin(), clusters.end(), [&p](auto&& a, auto&& b) { return (a-p).dot(a-p) < (b-p).dot(b-p); })
+					);
 
-				new_clusters[m] += p;
-				counter[m]++;
-				*it++ = clusters[m];
+			// f += (p-clusters[m]).dot(p-clusters[m]);
+			new_clusters[m] += p;
+			counter[m]++;
+			*it++ = clusters[m];
 		}
 
 		for (int j = 0; j < k; j++) {
@@ -55,13 +57,10 @@ int main()
 				new_clusters[j] /= counter[j];
 		}
 
-
-		unsigned eps = std::inner_product(clusters.begin(), clusters.end(), new_clusters.begin(), 0, std::plus<>(), [](auto&& lhs, auto&& rhs) { return (lhs-rhs).dot(lhs-rhs); });
-
-		std::cout << eps << std::endl; // strange convergence?!
-
-		if (eps < 5)
+		if (std::inner_product(clusters.begin(), clusters.end(), new_clusters.begin(), 0ull, std::plus<>(), [](auto&& lhs, auto&& rhs) { return (lhs-rhs).dot(lhs-rhs); }) < 5)
 			break;
+
+		// std::cout << f << std::endl;
 
 		clusters = std::move(new_clusters);
 	}
